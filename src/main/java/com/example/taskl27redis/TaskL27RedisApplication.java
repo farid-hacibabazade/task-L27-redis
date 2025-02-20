08a3ff2b434b548bbbf00da967da712998fdecfe
farid-hacibabazade <faridhaci0@gmail.com>
2025-02-20 22:38:07 +0400

    initial commit

---
+++ src/main/java/com/example/taskl27redis/TaskL27RedisApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskl27redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskL27RedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskL27RedisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/taskl27redis/TaskL27RedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskl27redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskL27RedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
